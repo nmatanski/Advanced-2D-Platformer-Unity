53485bf532ea107f754a7bd697360fd5ae5df696
nmatanski <nmatanski95@abv.bg>
2020-02-16 02:33:36 +0200

    Electric Particle, Shader Graph and material added; Added Hidden Room Tilemap which reveals hidden room on entering (trigger)

---
+++ 2D Platformer/Assets/Scripts/HiddenRoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Platformer
+{
+    public class HiddenRoom : MonoBehaviour
+    {
+        [SerializeField]
+        private bool isExplored = false;
+
+        private TilemapRenderer tilemapRenderer;
+        private Tilemap tilemap;
+
+        void Start()
+        {
+            tilemapRenderer = GetComponent<TilemapRenderer>();
+            tilemap = GetComponent<Tilemap>();
+
+            if (isExplored)
+            {
+                var color = tilemap.color;
+                tilemap.color = new Color(color.r, color.g, color.b, 0);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "Player" && !isExplored)
+            {
+                ///TODO: Explore secret room SFX
+                StartCoroutine(FadeTo(tilemap, 0, .3f));
+            }
+        }
+
+        private IEnumerator FadeTo(Tilemap tilemap, float aValue, float aTime)
+        {
+            var color = tilemap.color;
+            for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+            {
+                tilemap.color = new Color(color.r, color.g, color.b, Mathf.Lerp(color.a, aValue, t));
+                yield return null;
+            }
+
+            tilemapRenderer.enabled = false;
+            isExplored = true;
+        }
+    }
+}
